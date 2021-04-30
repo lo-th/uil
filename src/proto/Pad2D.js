78f11170a72b828fc0a2ce926d9a822e1f316c4c
takashi-tk <hubert.moliere@gmail.com>
2021-04-30 18:05:12 +0200

    Addition of Pad2D component
    Addition of Pad2D component - first version

--- src/proto/Pad2D.js
+++ src/proto/Pad2D.js
@@ -9,19 +9,30 @@ export class Pad2D extends Proto {
         super( o );
 
         this.autoWidth = false;
-
-        this.value = [ 0, 0 ];
+        this.margin = 15;
         this.pos = new V2();
-        this.bounds = { x1: -180, x2: 180, y1: -180, y2: 180 };
-        this.pos.x = this.w / 2;
-        this.pos.y = this.w / 2;
-        this.margin = 10;
+
+        this.model = o.stype || 0;
+        if( o.mode !== undefined ) this.model = o.mode;
+
+        this.precision = o.precision === undefined ? 2 : o.precision;
+
+        this.bounds = {};
+        this.bounds.x1 = o.x1 || -1;
+        this.bounds.x2 = o.x2 || 1;
+        this.bounds.y1 = o.y1 || -1;
+        this.bounds.y2 = o.y2 || 1;
 
         this.lerpX = this.lerp( this.margin, this.w - this.margin , this.bounds.x1, this.bounds.x2 );
         this.lerpY = this.lerp( this.margin, this.w - this.margin , this.bounds.y1, this.bounds.y2 );
 
-        this.precision = 0;
+        this.alerpX = this.lerp( this.bounds.x1, this.bounds.x2, this.margin, this.w - this.margin );
+        this.alerpY = this.lerp( this.bounds.y1, this.bounds.y2, this.margin, this.w - this.margin );
 
+        this.value = [];
+        let v = ( Array.isArray( o.value ) && o.value.length == 2 ) ? o.value : [ 0, 0 ];
+        this.setValue( v );
+        
         this.h = o.h || this.w + 10;
         this.top = 0;
 
@@ -43,15 +54,24 @@ export class Pad2D extends Proto {
 
         // Pad
         let svg = Tools.dom( 'svg', Tools.css.basic , { viewBox: '0 0 ' + this.w + ' ' + this.w, width: this.w, height: this.w, preserveAspectRatio: 'none' } );
-        Tools.dom( 'rect', '', { x: this.margin, y: this.margin, width: this.w - 20, height: this.w - 20, stroke:'rgba(0,0,0,0.25)', 'stroke-width':0, fill: 'rgba(0,0,0,0.1)' }, svg ); // 0
+        Tools.dom( 'rect', '', { x: this.margin - 5, 
+                                 y: this.margin - 5, 
+                                 width: this.w - ( this.margin - 5 ) * 2 , 
+                                 height: this.w - ( this.margin -5 ) * 2 ,
+                                 rx: 5, ty: 5, 
+                                 stroke:'rgba(0,0,0,0.25)', 
+                                 'stroke-width':0, 
+                                 fill: 'rgba(0,0,0,0.1)' }, svg ); // 0
+
+        Tools.dom( 'rect', '', { x: this.margin, y: this.margin, width: this.w - this.margin * 2, height: this.w - this.margin * 2, stroke:'rgba(0,0,0,0.25)', 'stroke-width':0, fill: 'rgba(0,0,0,0.1)' }, svg ); // 1
         this.c[3] = svg;
         this.setSvg( this.c[3], 'viewBox', '0 0 ' + this.w + ' ' + this.w );
         this.setCss( this.c[3], { left: 0, top: this.top } );
 
         // Pointer
-        Tools.dom( 'line', '', { x1: this.margin, y1: this.w / 2, x2: this.w - this.margin, y2: this.w / 2, stroke:'rgba(1,1,1,0.25)', 'stroke-width':1 }, this.c[3] ); // 1
-        Tools.dom( 'line', '', { x1: this.w / 2, y1: this.margin, x2: this.w / 2, y2: this.w - this.margin, stroke:'rgba(1,1,1,0.25)', 'stroke-width':1 }, this.c[3] ); // 2
-        Tools.dom( 'circle', '', { cx: this.w / 2, cy: this.w / 2, r: 5, stroke:'rgba(0,0,0,0.25)', 'stroke-width':0, fill: this.fontColor }, this.c[3] ); // 3
+        Tools.dom( 'line', '', { x1: this.margin, y1: this.w / 2, x2: this.w - this.margin, y2: this.w / 2, stroke:'#1C1C1C', 'stroke-width': 1 }, this.c[3] ); // 2
+        Tools.dom( 'line', '', { x1: this.w / 2, y1: this.margin, x2: this.w / 2, y2: this.w - this.margin, stroke:'#1C1C1C', 'stroke-width': 1 }, this.c[3] ); // 3
+        Tools.dom( 'circle', '', { cx: this.w / 2, cy: this.w / 2, r: 5, stroke: 'rgba(0,0,0,0.25)', 'stroke-width': 0, fill: this.fontColor }, this.c[3] ); // 4
 
         this.init();
         
@@ -92,8 +112,15 @@ export class Pad2D extends Proto {
 
         if( !this.isDown ) return;
 
-        this.pos.x = e.clientX - this.zone.x;
-        this.pos.y = e.clientY - this.zone.y - this.top;
+        let x = e.clientX - this.zone.x;
+        let y = e.clientY - this.zone.y - this.top;
+
+        if( x < this.margin ) x = this.margin;
+        if( x > this.w - this.margin ) x = this.w - this.margin;
+        if( y < this.margin ) y = this.margin;
+        if( y > this.w - this.margin ) y = this.w - this.margin;
+
+        this.setPos( [ x , y ] );
         
         this.update();
 
@@ -103,15 +130,9 @@ export class Pad2D extends Proto {
 
         if( up === undefined ) up = true;
         
-        let x = this.pos.x;
-        let y = this.pos.y;
+        this.c[2].textContent = this.value;
 
-        if( x < this.margin ) x = this.margin;
-        if( x > this.w - this.margin ) x = this.w - this.margin;
-        if( y < this.margin ) y = this.margin;
-        if( y > this.w - this.margin ) y = this.w - this.margin;
-   
-        this.setValue( [ x , y ] );
+        this.updateSVG();
 
         if( up ) this.send();
 
@@ -119,31 +140,47 @@ export class Pad2D extends Proto {
 
     updateSVG() {
 
-        let x = this.pos.x;
-        let y = this.pos.y;
+        if ( this.model == 1 ) {
 
-        this.setSvg( this.c[3], 'y1', y, 1 );
-        this.setSvg( this.c[3], 'y2', y, 1 );
+            this.setSvg( this.c[3], 'y1', this.pos.y, 2 );
+            this.setSvg( this.c[3], 'y2', this.pos.y, 2 );
 
-        this.setSvg( this.c[3], 'x1', x, 2 );
-        this.setSvg( this.c[3], 'x2', x, 2 );
+            this.setSvg( this.c[3], 'x1', this.pos.x, 3 );
+            this.setSvg( this.c[3], 'x2', this.pos.x, 3 );
 
-        this.setSvg( this.c[3], 'cx', x, 3 );
-        this.setSvg( this.c[3], 'cy', y, 3 );
+        }
+
+        this.setSvg( this.c[3], 'cx', this.pos.x, 4 );
+        this.setSvg( this.c[3], 'cy', this.pos.y, 4 );
+
     }
 
-    setValue ( v ) {
+    setPos ( p ) {
+
+        if( p === undefined ) p = [ this.w / 2, this.w / 2 ];
+
+        this.pos.set( p[0] , p[1] );
+
+        this.value[0] = this.lerpX( p[0] ).toFixed( this.precision );
+        this.value[1] = this.lerpY( p[1] ).toFixed( this.precision );
+
+    }
+
+    setValue ( v, up = false ) {
 
         if( v === undefined ) v = [ 0, 0 ];
 
-        this.pos.set( v[0] , v[1] );
+        if ( v[0] < this.bounds.x1 ) v[0] = this.bounds.x1;
+        if ( v[0] > this.bounds.x2 ) v[0] = this.bounds.x2;
+        if ( v[1] < this.bounds.y1 ) v[1] = this.bounds.y1;
+        if ( v[1] > this.bounds.y2 ) v[1] = this.bounds.y2;
 
-        this.value[0] = this.lerpX( v[0] ).toFixed( this.precision );
-        this.value[1] = this.lerpY( v[1] ).toFixed( this.precision );
+        this.value[0] = v[0];
+        this.value[1] = v[1];
 
-        this.c[2].textContent = this.value;
+        this.pos.set( this.alerpX( v[0] ) , this.alerpY( v[1] ) );
 
-        this.updateSVG();
+       if ( up ) this.update();
 
     }
 
